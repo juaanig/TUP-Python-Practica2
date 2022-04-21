e04287b3349cff4add7dd965035f1ccff980c6ec
LucasButto <lbutto40@gmail.com>
2022-04-21 18:31:06 -0300

    Ejercicio 4 resuelto

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -13,7 +13,20 @@ def es_vocal_if(letra: str) -> bool:
 
     Referencia: https://docs.python.org/3/library/stdtypes.html#string-methods
     """
+    letra=letra.lower()
 
+    if letra == "a":
+        return True
+    if letra == "e":
+        return True
+    if letra == "i":
+        return True
+    if letra == "o":
+        return True
+    if letra == "u":
+        return True
+    if letra != "a" or letra != "e" or letra != "i" or letra != "o" or letra != "u":
+        return False
 
 # NO MODIFICAR - INICIO
 assert es_vocal_if("a")
@@ -39,7 +52,14 @@ def es_vocal_if_in(letra: str) -> bool:
 
     Referencia: https://docs.python.org/3/reference/expressions.html#membership-test-operations # noqa: E501
     """
+    vocales="a","e","i","o","u"
 
+    letra=letra.lower()
+
+    if letra in vocales:
+        return True
+    
+    return False
 
 # NO MODIFICAR - INICIO
 assert es_vocal_if_in("a")
@@ -60,7 +80,14 @@ def es_vocal_in(letra: str) -> bool:
         - No utilizar FOR.
         - No utilizar listas.
     """
+    vocales="a","e","i","o","u"
+    letra=letra.lower()
 
+    resultado= False
+
+    resultado= letra in vocales
+
+    return resultado
 
 # NO MODIFICAR - INICIO
 assert es_vocal_in("a")
